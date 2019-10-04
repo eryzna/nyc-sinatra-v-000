69a5133eef067c4a142d4888197838bbf0b2f986
Elana Ryznar <eryzna@gmail.com>
2019-10-04 22:34:24 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,7 @@
 class CreateLandmarks < ActiveRecord::Migration
-  raise 'Write CreateLandmarks migration here'
+  def change
+    create_table :figures do |t|
+      t.string :name
+  end
+    
 end
